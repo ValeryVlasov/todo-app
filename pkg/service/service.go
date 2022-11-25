c46630b7ccc3e8d167fcae38ceb81cf97752f170
Valery <valera.vlasov11@mail.ru>
2022-11-25 18:44:35 +0300

    CRUD-operations with list items

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -12,7 +12,7 @@ type Authorization interface {
 }
 
 type TodoList interface {
-	Create(userid int, list todo_app.TodoList) (int, error)
+	Create(userId int, list todo_app.TodoList) (int, error)
 	GetAll(userId int) ([]todo_app.TodoList, error)
 	GetById(userId, listId int) (todo_app.TodoList, error)
 	Delete(userId, listId int) error
@@ -20,6 +20,11 @@ type TodoList interface {
 }
 
 type TodoItem interface {
+	Create(userId, listId int, item todo_app.TodoItem) (int, error)
+	GetAll(userId, listId int) ([]todo_app.TodoItem, error)
+	GetById(userId, itemId int) (todo_app.TodoItem, error)
+	Delete(userId, itemId int) error
+	Update(userId, itemId int, input todo_app.UpdateItemInput) error
 }
 
 type Service struct {
@@ -32,5 +37,6 @@ func NewService(repos *repository.Repository) *Service {
 	return &Service{
 		Authorization: NewAuthService(repos.Authorization),
 		TodoList:      NewTodoListService(repos.TodoList),
+		TodoItem:      NewTodoItemService(repos.TodoItem, repos.TodoList),
 	}
 }
