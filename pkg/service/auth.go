c8277121dbf404d0140a57aa1c916bba720be08c
Valery <valera.vlasov11@mail.ru>
2022-11-25 14:24:09 +0300

    user registration added

---
+++ pkg/service/auth.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"crypto/sha1"
+	"fmt"
+	todo_app "todoApp"
+	"todoApp/pkg/repository"
+)
+
+const salt = "adivh4hec1328hchiucshadi"
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo_app.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
